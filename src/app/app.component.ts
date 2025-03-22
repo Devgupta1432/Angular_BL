f9fd6c0983f8bea8c89807b19caf4b64402da98d
Devgupta1432 <devgupta791@gmail.com>
2025-03-22 15:25:54 +0530

    link added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloworld-angular';
-  bridgeLabzLogo = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20Brand%20Logo.svg';
+  title = 'Hello from BridgeLabz';
+  bridgeLabzLogo = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20Brand%20Logo.svg'; // Logo URL
+  bridgeLabzURL = 'https://www.bridgelabz.com'; // Website URL
+
+  openBridgeLabzSite() {
+    window.open(this.bridgeLabzURL, '_blank'); // Opens in a new tab
+  }
 }
